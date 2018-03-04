4caad068ed7d2807c446cd889525a82ea85e4c83
Joopakerboom <31732324+Joopakerboom@users.noreply.github.com>
2018-03-04 14:12:27 +0100

    Films verschillende interacties - Test 01
    Na de basis van de detail pagina verder ontworpen te hebben zijn er ook meerder vormen van interactie toegevoegd. De gebruiker kan nu door op de pijltjes te klikken meer images zien, de pijltjes toetsen kan hier ook voor ingezet worden en op mobiel is swipen ook een interactie mogelijkheid.

--- opdracht2/filmsinteractietest01/js/imagescroll.js
+++ opdracht2/filmsinteractietest01/js/imagescroll.js
@@ -9,10 +9,12 @@ var nextButton = document.querySelector('main.detailpage section>div:last-of-typ
 
 function showImages(n) {
   if (n > movieImages.length) {
-      slideIndex = 1;}   
+      slideIndex = 1;
+  }   
     
   if (n < 1) {
-      slideIndex = movieImages.length;}
+      slideIndex = movieImages.length;
+  }
     
   for (i = 0; i < movieImages.length; i++) {
      movieImages[i].style.display = "none";  
@@ -31,7 +33,63 @@ prevButton.addEventListener("click", function(){
     plusDivs(-1);
 }, false);
 
+window.addEventListener('keydown', function(e) {
+    if (e.keyCode === 37) {
+        plusDivs(-1);
+    }
+}, false);
+
 nextButton.addEventListener("click", function(){
     plusDivs(1);
 }, false);
 
+window.addEventListener('keydown', function(e) {
+    if (e.keyCode === 39) {
+        plusDivs(1);
+    }
+}, false);
+
+
+///////////////////////////
+
+var touchstartX = 0;
+var touchstartY = 0;
+var touchendX = 0;
+var touchendY = 0;
+
+var swipeZone = document.querySelector('main.detailpage section');
+
+function handleGesture() {
+    if (touchendX <= touchstartX) {
+        console.log('Swiped left');
+        plusDivs(1);
+    }
+    
+    if (touchendX >= touchstartX) {
+        console.log('Swiped right');
+        plusDivs(-1);
+    }
+    
+    if (touchendY <= touchstartY) {
+        console.log('Swiped up');
+    }
+    
+    if (touchendY >= touchstartY) {
+        console.log('Swiped down');
+    }
+    
+    if (touchendY === touchstartY) {
+        console.log('Tap');
+    }
+}
+
+swipeZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+swipeZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false); 
