821d3b133e024eb3450f1bbc06f15b0b89342c50
Joopakerboom <31732324+Joopakerboom@users.noreply.github.com>
2018-03-05 14:09:03 +0100

    Films verschillende interacties - Test 01
    Een update die wat kleine dingetjes opschoont zowel visueel als in de code.

--- opdracht2/filmsinteractietest01/js/imagescroll.js
+++ opdracht2/filmsinteractietest01/js/imagescroll.js
@@ -1,34 +1,46 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
-
+// VARIABLES CAROUSEL SLIDES
 var i = 0;
-var slideIndex = 1;
+var slideIndex = 1; // Begin bij eerste image
 var movieImages = document.querySelectorAll('main.detailpage section>div:last-of-type>img');
 var prevButton = document.querySelector('main.detailpage section>div:last-of-type>button:nth-of-type(1)');
 var nextButton = document.querySelector('main.detailpage section>div:last-of-type>button:nth-of-type(2)');
 
+// VARIABLES TOUCH/ SWIPE GESTURE
+var touchstartX = 0;
+var touchendX = 0;
+var swipeZone = document.querySelector('main.detailpage section');
+
+
+// FUNCTION OM IMAGES TE LATEN ZIEN
 function showImages(n) {
   if (n > movieImages.length) {
       slideIndex = 1;
+      // Als n groter wordt dan de totale image array dan moet die resetten naar slideIndex 1.
   }   
     
   if (n < 1) {
       slideIndex = movieImages.length;
+      // Als n kleiner wordt dan 1, dan moet de slideIndex het cijfer worden van de image uit de array. Het is voornamelijk voor de terug knop.
   }
     
   for (i = 0; i < movieImages.length; i++) {
      movieImages[i].style.display = "none";  
+      // De vorige image die actief was moet terug gezet worden op display none.
   }
   
-  movieImages[slideIndex-1].style.display = "block"; 
+  movieImages[slideIndex-1].style.display = "block"; // Laat de image zien uit de array met -1 anders wordt de eerste overgeslagen.
 }
 
-showImages(slideIndex);
+showImages(slideIndex); // Om de eerste image al zichtbaar te maken.
 
 function plusDivs(n) {
   showImages(slideIndex += n);
+    console.log(slideIndex);
 }
 
+// EVENTLISTENER DIE LUISTERT NAAR EEN CLICK OF KEYDOWN
 prevButton.addEventListener("click", function(){
     plusDivs(-1);
 }, false);
@@ -50,16 +62,8 @@ window.addEventListener('keydown', function(e) {
 }, false);
 
 
-///////////////////////////
-
-var touchstartX = 0;
-var touchstartY = 0;
-var touchendX = 0;
-var touchendY = 0;
-
-var swipeZone = document.querySelector('main.detailpage section');
-
-function handleGesture() {
+// FUNCTION DIE BEPAALD WELKE KANT ER OP GESWIPED WORDT
+function swipeGesture() {
     if (touchendX <= touchstartX) {
         console.log('Swiped left');
         plusDivs(1);
@@ -69,27 +73,14 @@ function handleGesture() {
         console.log('Swiped right');
         plusDivs(-1);
     }
-    
-    if (touchendY <= touchstartY) {
-        console.log('Swiped up');
-    }
-    
-    if (touchendY >= touchstartY) {
-        console.log('Swiped down');
-    }
-    
-    if (touchendY === touchstartY) {
-        console.log('Tap');
-    }
 }
 
+// EVENTLISTENER DIE DE SWIPE REGISTREERT
 swipeZone.addEventListener('touchstart', function(event) {
     touchstartX = event.changedTouches[0].screenX;
-    touchstartY = event.changedTouches[0].screenY;
 }, false);
 
 swipeZone.addEventListener('touchend', function(event) {
     touchendX = event.changedTouches[0].screenX;
-    touchendY = event.changedTouches[0].screenY;
-    handleGesture();
+    swipeGesture();
 }, false); 
